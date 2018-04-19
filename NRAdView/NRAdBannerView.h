5cd462c3a7fc6e0d8fdd31e3fcf6046b74021e34
BabyBus <504010900@qq.com>
2018-04-19 17:00:44 +0800

    adjust something

--- NRAdView/NRAdBannerView.h
+++ NRAdView/NRAdBannerView.h
@@ -9,6 +9,15 @@
 #import <UIKit/UIKit.h>
 #import "NRAdView.h"
 
+extern NSString *_Nonnull const NRAdBannerViewPlayOrPauseNotification;
+extern NSString *_Nonnull const NRAdBannerViewPlayOrPauseKey;
+extern NSString *_Nonnull const NRAdBannerViewTagKey;
+
 @interface NRAdBannerView : UIView
-- (instancetype)initWithFrame:(CGRect)frame withData:(NSArray*)arrayData withDelegate:(id<NRAdViewDelegate>)delegate;
+@property (nonatomic, weak) id<NRAdViewDelegate> delegate;
+
+- (instancetype)initWithFrame:(CGRect)frame withData:(NSArray *)arrayData withDelegate:(id<NRAdViewDelegate>)delegate;
+
+- (void)reloadWithDataArray:(NSArray *)adArray;
+
 @end
--- NRAdView/NRAdBannerView.m
+++ NRAdView/NRAdBannerView.m
@@ -8,20 +8,24 @@
 
 #import "NRAdBannerView.h"
 
-@interface NRAdBannerView()
-@property(nonatomic, strong) NRAdView *adView;
-@end;
+NSString *const NRAdBannerViewPlayOrPauseNotification = @"NRAdBannerViewPlayOrPauseNotification";
+NSString *const NRAdBannerViewPlayOrPauseKey = @"NRAdBannerViewPlayOrPauseKey";
+NSString *const NRAdBannerViewTagKey = @"NRAdBannerViewTagKey";
+
+@interface NRAdBannerView ()
+@property (nonatomic, strong) NRAdView *adView;
+@end
+;
 
 @implementation NRAdBannerView
 
-- (instancetype)initWithFrame:(CGRect)frame withData:(NSArray*)arrayData withDelegate:(id<NRAdViewDelegate>)delegate
-{
+- (instancetype)initWithFrame:(CGRect)frame withData:(NSArray *)arrayData withDelegate:(id<NRAdViewDelegate>)delegate {
     self = [super initWithFrame:frame];
     if (self) {
         NRAdView *adView = [[NRAdView alloc] initWithBuilder:^(NRAdViewBuilder *builder) {
             builder.adArray = arrayData;
-            builder.viewFrame =CGRectMake(0, 0, frame.size.width, frame.size.height);
-            builder.adItemSize = (CGSize){frame.size.width*(670/750.0)*(1/1.1),frame.size.width*(670/750.0)*0.9*(306/670.0) };
+            builder.viewFrame = CGRectMake(0, 0, frame.size.width, frame.size.height);
+            builder.adItemSize = (CGSize){frame.size.width * (670 / 750.0) * (1 / 1.1), frame.size.width * (670 / 750.0) * 0.9 * (306 / 670.0)};
             builder.minimumLineSpacing = 12;
             builder.secondaryItemMinAlpha = 0.6;
             builder.threeDimensionalScale = 1.1;
@@ -29,25 +33,42 @@
             builder.allowedInfinite = YES;
             builder.infiniteCycle = 4.0f;
         }];
-        adView.backgroundColor = [UIColor whiteColor];//[UIColor colorWithWhite:0.95 alpha:0.2];
+        adView.backgroundColor = [UIColor whiteColor]; //[UIColor colorWithWhite:0.95 alpha:0.2];
         adView.delegate = delegate;
         [adView play];
         _adView = adView;
         [self addSubview:adView];
         self.backgroundColor = [UIColor grayColor];
+        
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handlePlayOrPause:) name:NRAdBannerViewPlayOrPauseNotification object:nil];
     }
     return self;
 }
 
-//#pragma mark NRAdViewDelegate
-//-(void)nr_scrollToIndex:(NSInteger)index
-//{
-//
-//}
-//
-//-(void)nr_didClickAd:(id)adModel
-//{
-//
-//}
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:NRAdBannerViewPlayOrPauseNotification object:nil];
+}
+
+- (void)reloadWithDataArray:(NSArray *)adArray {
+    [self.adView reloadWithDataArray:adArray];
+}
+
+- (void)setDelegate:(id<NRAdViewDelegate>)delegate {
+    self.adView.delegate = delegate;
+}
+
+#pragma mark - notification
+
+- (void)handlePlayOrPause:(NSNotification *)no {
+    NSInteger viewTag = [[[no userInfo] objectForKey:NRAdBannerViewTagKey] integerValue];
+    BOOL bPlay = [[[no userInfo] objectForKey:NRAdBannerViewPlayOrPauseKey] boolValue];
+    if (self.tag == viewTag) {
+        if (bPlay) {
+            [self.adView play];
+        } else {
+            [self.adView pause];
+        }
+    }
+}
 
 @end
